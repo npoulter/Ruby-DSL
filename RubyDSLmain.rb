493292702278d976e63b02a683378214651c286f
n p <n p>
2016-09-24 15:41:13 -0600

    DSL Program now initializes, prints the menu, and loads/processes the file

--- RubyDSLmain.rb
+++ RubyDSLmain.rb
@@ -8,12 +8,17 @@
 
 class RubyDSL
 	
+	def initializeDSL
+		@fileHash = Hash.new
+	end
+	
 	def printMainMenu
 		puts "<<<<<  Main Menu >>>>>"
 		puts "1. Load rules"
 		puts "2. Process Orders"
 		puts "3. End"
 		puts "Your option: "
+		# Only take the first character
 		userSelection = gets.chomp.chars.first
 	end
 	
@@ -23,13 +28,37 @@ class RubyDSL
 	end
 	
 	def loadFile(fileName)
+		# Check that the file name has the .txt extension
 		if fileName =~ /\.txt/
 			fileName = fileName
 		else
 			fileName = fileName + ".txt"
 		end
+		
+		# Open the file and iterate through it unless it is invalid
 		if File.file?(fileName)
 			@RuleFile = File.open(fileName.to_s)
+			x = 0
+			theCommand = ""
+			theProduct = ""
+			# Iterate line by line
+			while line = @RuleFile.gets
+				# Split the line into words
+				line.split(' ').each do |word|
+					if x % 2 == 0
+						# First word is the product
+						puts word
+						theProduct = word
+						x = x + 1
+					else
+						# Second word is what we're doing with it
+						theCommand = word
+						# Store it in the hash
+						@fileHash[theProduct.to_sym] = theCommand.to_sym
+						x = x + 1
+					end
+				end
+			end
 		else
 			puts "Invalid filename! Please try again."
 		end
@@ -37,5 +66,6 @@ class RubyDSL
 end
 
 DSL = RubyDSL.new
+DSL.initializeDSL
 DSL.printMainMenu
 DSL.loadFile(DSL.getFileName)
