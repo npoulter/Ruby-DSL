aa12039dc0d648b5b7bd006ac00fcddf2c3e64ff
n p <n p>
2016-09-24 14:50:07 -0600

    Made a couple minor additions

--- RubyDSLmain.rb
+++ RubyDSLmain.rb
@@ -6,14 +6,36 @@
 	Project: Ruby DSL Main file
 =end
 
-class JustDoIt
+class RubyDSL
+	
 	def printMainMenu
 		puts "<<<<<  Main Menu >>>>>"
 		puts "1. Load rules"
 		puts "2. Process Orders"
 		puts "3. End"
+		puts "Your option: "
+		userSelection = gets.chomp.chars.first
+	end
+	
+	def getFileName
+		puts "Enter file name containing the rules: "
+		fileName = gets.chomp
+	end
+	
+	def loadFile(fileName)
+		if fileName =~ /\.txt/
+			fileName = fileName
+		else
+			fileName = fileName + ".txt"
+		end
+		if File.file?(fileName)
+			@RuleFile = File.open(fileName.to_s)
+		else
+			puts "Invalid filename! Please try again."
+		end
 	end
 end
 
-doIt = JustDoIt.new
-doIt.printMainMenu
+DSL = RubyDSL.new
+DSL.printMainMenu
+DSL.loadFile(DSL.getFileName)
