045a16c9bf5cd3456c038a18930bf8607a79ae00
Jess Boerner <jessboerner@gmail.com>
2016-09-26 02:27:38 -0600

    Add basic product class
    Conforms to standard of in class quiz example

--- product.rb
+++ product.rb
@@ -1,21 +1,26 @@
-Class Product
+# A product is a named collection of jobs
+class Product
+	attr_reader :name, :jobs
 
+	def initialize(name)
+		@name = name.split('-').join(' ')
+		@jobs = []
+	end
 
+	def <<(job)
+		@jobs << job
+	end
 
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# A job is a unique object with a type (to be called as a method)
+# & an optional text that pertains to the type
+class Job
+	attr_reader :type, :text
+	
+	def initialize(type, text)
+		@type = type
+		@text = text.split('-').join(' ') if text else text
+	end
 
 end
